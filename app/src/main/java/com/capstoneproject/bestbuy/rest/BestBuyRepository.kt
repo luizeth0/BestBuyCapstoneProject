fdfba0170904e5cf95acf04ea34db552424de6ef
luizeth0 <luis.hemi94@gmail.com>
2023-03-04 23:35:02 -0500

    Added Repository & utils(UIState/CustomExceptions)

---
+++ app/src/main/java/com/capstoneproject/bestbuy/rest/BestBuyRepository.kt
@@ -0,0 +1,37 @@
+package com.capstoneproject.bestbuy.rest
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.capstoneproject.bestbuy.model.products.ProductsResponse
+import com.capstoneproject.bestbuy.utils.FailureResponse
+import com.capstoneproject.bestbuy.utils.NullResponse
+import com.capstoneproject.bestbuy.utils.UIState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+interface BestBuyRepository {
+    fun getProducts(): Flow<UIState<ProductsResponse>>
+}
+
+class BestBuyRepositoryImpl(
+    private val bestBuyApi: BestBuyApi
+): BestBuyRepository {
+
+    override fun getProducts(): Flow<UIState<ProductsResponse>> = flow {
+        emit(UIState.LOADING)
+        try {
+            val response = bestBuyApi.getProducts()
+            Log.d(TAG,"getProducts: $response")
+            if (response.isSuccessful) {
+                response.body()?.let {
+                    emit(UIState.SUCCESS(it))
+                } ?: NullResponse()
+            } else {
+                throw FailureResponse(response.errorBody()?.string())
+            }
+        } catch (e: Exception) {
+            emit(UIState.ERROR(e))
+        }
+    }
+
+}
